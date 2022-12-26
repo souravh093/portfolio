b2fa57a871c81501f40e73e9d1f4843516db633e
Sourav halder <54834807+souravh093@users.noreply.github.com>
2022-12-26 21:02:03 +0600

    protfolio site upload
    first commit 2022

---
+++ custom.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menuToggle = document.querySelector('.toggle');
+    const sidebar = document.querySelector('.sidebar');
+    menuToggle.classList.toggle('active');
+    sidebar.classList.toggle('active');
+}
+
